631b28ff882807a49fad4705e92653d27175e743
marceloexc <marceloexc@tutanota.de>
2024-05-26 17:31:05 -0500

    testing hardcoded database models

---
+++ src/models.py
@@ -0,0 +1,41 @@
+# models.py
+
+from sqlalchemy import Column, Integer, String, JSON, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class Directory(Base):
+    __tablename__ = "directories"
+
+    hoga_id = Column(Integer, primary_key=True, index=True)
+    title = Column(String(200), nullable=True)
+    extractor_type = Column(String(50), nullable=True)
+    posts = relationship('Post', backref="directory", lazy='selectin')
+
+
+class Post(Base):
+    __tablename__ = "posts"
+
+    post_id = Column(Integer, primary_key=True, index=True)
+    internal_file_directory = Column(String(200), nullable=True)
+    media_filenames = Column(JSON, nullable=True)
+    post_content = Column(String(900), nullable=True)
+    post_type = Column(String(30), nullable=True)
+    post_platform = Column(String(30), nullable=True)
+    post_published_timestamp = Column(String(30), nullable=True)
+    post_author_handle = Column(String(30), nullable=True)
+    post_author_username = Column(String(30), nullable=True)
+    post_repost_id = Column(String(30), nullable=True)
+    post_media_attachments = Column(String(30), nullable=True)
+    post_like_count = Column(String(30), nullable=True)
+    post_repost_count = Column(String(30), nullable=True)
+    post_bookmark_count = Column(String(30), nullable=True)
+    post_comments_count = Column(String(30), nullable=True)
+    repost_original_post_id = Column(String(30), nullable=True)
+    post_privacy_settings = Column(String(30), nullable=True)
+    test_filename = Column(JSON, nullable=True)
+
+    directory_id = Column(Integer, ForeignKey('directories.hoga_id'), nullable=False)

6a13cd9be4e66dabc47d66a4abb0ba0c08a54006
marceloexc <marceloexc@tutanota.de>
2024-07-02 16:46:13 -0500

    change from uvicorn to Hypercorn

--- src/main.py
+++ src/main.py
@@ -1,25 +1,13 @@
-import sys
+import asyncio
 
-import uvicorn
-from uvicorn.config import LOGGING_CONFIG
+from hypercorn.asyncio import serve
+from hypercorn.config import Config
 
 from src.app import app
 
-from typing import Any, Dict
-
-import logging
-
 if __name__ == "__main__":
-    # https://github.com/tiangolo/fastapi/discussions/7457
+    config = Config()
+    config.bind = ["0.0.0.0:5125"]
+    config.loglevel = "debug"
 
-    # logger = logging.getLogger(__name__)
-    # logger.setLevel(logging.DEBUG)
-    # stream_handler = logging.StreamHandler(sys.stdout)
-    # log_formatter = logging.Formatter("%(asctime)s [%(levelname)s] %(name)s: %(message)s")
-    # stream_handler.setFormatter(log_formatter)
-    # logger.addHandler(stream_handler)
-    #
-    # logger.info(
-    #     'API is starting up')  #https://stackoverflow.com/questions/77001129/how-to-configure-fastapi-logging-so-that-it-works-both-with-uvicorn-locally-and
-
-    uvicorn.run(app, host="0.0.0.0", port=5125, log_level="debug")
+    asyncio.run(serve(app, config))

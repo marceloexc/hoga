cb18a1f10f95cfcc976c4379d870c1208e767895
marceloexc <marceloexc@tutanota.de>
2024-07-26 12:10:29 -0500

    backend: chores (schema and cleanup)

--- src/main.py
+++ src/main.py
@@ -5,9 +5,5 @@ from uvicorn.config import LOGGING_CONFIG
 
 from src.app import app
 
-from typing import Any, Dict
-
-import logging
-
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=5125, log_level="debug")

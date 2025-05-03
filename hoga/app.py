2d65e6c3d27b00346bdf72e48edb5a0fb780031a
marceloexc <marceloexc@tutanota.de>
2025-05-03 16:30:16 -0500

    no surprises

--- hoga/app.py
+++ hoga/app.py
@@ -2,18 +2,14 @@ import logging
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-from hoga.database import engine
-from hoga.models import Base
-from hoga.routes.media import media
-from hoga.routes.api import api_v1
-from hoga.routes.routes import router
+
 from hoga.utils.compile_scss import compile_scss
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     # startup routines
-    compile_scss()
+    # compile_scss()
     yield
 
 
@@ -23,15 +19,4 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 logger.info("Logger initalized.")
-
-# Include routers
-app.include_router(router)
-app.include_router(api_v1, prefix="/api")
-app.include_router(media, prefix="/media")
-
-# Create database columns
-Base.metadata.create_all(bind=engine)
-
-# Mount static files
-app.mount("/static", StaticFiles(directory="hoga/static"), name="static")
 
